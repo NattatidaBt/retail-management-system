7b89f96622a24f9b60e1a575b8dbf9a392631fa6
NattatidaBt <nattatida16658@gmail.com>
2026-03-06 03:54:31 +0700

    update backend API

--- back-end/app.js
+++ back-end/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const authRoutes = require('./routes/auth.routes');
-const postRoutes = require('./routes/post.routes');
+const productRoutes = require('./routes/product.routes');
+const orderRoutes = require('./routes/order.routes');  
 const path = require('path');
 require('dotenv').config();
-const supabase = require('./config/supabase'); // Import our database connection
-const app = express(); // Create Express application
-app.use(express.json()); // Update app.js to allow Express to handle JSON requests
+const supabase = require('./config/supabase');
+
+const app = express();
+app.use(express.json());
+
 app.use("/auth", authRoutes);
-app.use("/posts", postRoutes);
-// Home page route
+app.use("/products", productRoutes);  
+app.use("/orders", orderRoutes);      
+
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to My First Authentication Backend App!</h1>`);
 });
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
